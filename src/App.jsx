f6a03219b33db8d0098f0cdb4cbe201822c6debe
Vinicius Gabriel Castro <vini.gabriell@outlook.com>
2022-07-07 02:06:33 -0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,39 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import logo from './assets/logo.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <form>
+      <img src={logo} alt="logo" />
+
+      <label>
+        Nome
+        <input type="text" />
+
+        Cpf
+        <input type="text" />
+
+        E-Mail
+        <input type="text" />
+
+        Data de Nascimento
+        <input type="text" />
+
+        Gênero
+        <input type="text" />
+
+        Senha
+        <input type="text" />
+
+        Confirmar Senha
+        <input type="text" />
+
+        <button type="submit">Cadastrar</button>
+        <button type="submit">Cancelar</button>
+      </label>
+    </form>
   )
 }
 
